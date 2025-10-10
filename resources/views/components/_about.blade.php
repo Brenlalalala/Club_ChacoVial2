0d4be4495eeb7d4055ba4b01f94593dd58414817
Ayelén Escalante <daianaayelenescalante@gmail.com>
2025-10-09 21:45:13 -0300

    Se crea la seccion about.

---
+++ resources/views/components/_about.blade.php
@@ -0,0 +1,16 @@
+<section id="nosotros" class="py-16 bg-white">
+    <div class="container mx-auto px-4">
+        <h2 class="text-3xl md:text-4xl font-bold mb-6 text-center">Nuestra Historia y Misión</h2>
+                <p class="text-lg text-gray-700 max-w-4xl mx-auto">
+                Desde su fundación en 1950, el Club Chaco Vial ha sido un pilar en la comunidad de Resistencia, un hogar para la pasión por el deporte y la sana convivencia. Lo que comenzó como un modesto punto de encuentro para entusiastas se ha transformado, a lo largo de las décadas, en una institución que simboliza la tradición, el esfuerzo y el crecimiento.
+                </p>
+                <p class="text-lg text-gray-700 max-w-4xl mx-auto">
+                Somos una institución dedicada a fomentar el deporte, la integración social y el bienestar de nuestros socios y la comunidad. Con una rica historia y un inquebrantable espíritu de camaradería, nos enorgullece ofrecer un entorno seguro y vibrante donde generaciones enteras pueden disfrutar, formar lazos de amistad y alcanzar sus metas.
+                </p>
+                <p class="text-lg text-gray-700 max-w-4xl mx-auto">
+                Más que un club, somos una gran familia unida por el amor al deporte y los valores que nos definen. Ven y sé parte del legado que construimos juntos, día a día, desde hace más de 70 años.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
