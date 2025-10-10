b71095b759ec5544d0920b18055e6b3357bb3f33
Ayelén Escalante <daianaayelenescalante@gmail.com>
2025-10-09 21:39:06 -0300

    Se crea el footer

---
+++ resources/views/components/_footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-gray-900 text-gray-400 p-6 text-center">
+    <div class="container mx-auto">
+        <p class="mt-4 text-sm">&copy; {{ date('Y') }} Club Chaco Vial. Todos los derechos reservados.</p>
+    </div>
+</footer>
